b563a070d29eb482686191625852b0c3bffe4d37
Federico Barbieri <federicobarbieri54@gmail.com>
2023-11-16 19:00:07 +0100

    init

---
+++ app.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import Account from '@/pages/Account.vue'
+import { supabase } from './lib/supabaseClient.js'
+
+const session = ref()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
+</script>
+
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <nuxtLayout>
+
+      <ul>
+      <li><nuxt-link to="/">HOME</nuxt-link></li>
+      <li><nuxt-link to="/Account">account</nuxt-link></li>  
+      </ul>
+
+        <Account v-if="session" :session="session" />
+        <nuxtPage v-else />
+
+      </nuxtLayout>
+      
+  </div>
+</template>
---
+++ pages/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h2>About Page</h2>
+      <p>This is the About page content.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About'
+  }
+  </script>
