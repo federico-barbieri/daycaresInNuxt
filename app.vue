f8b3038405b5ced45ff80a2fc34f65a315e1037c
Federico Barbieri <federicobarbieri54@gmail.com>
2023-11-16 23:11:28 +0100

    sidekick

--- app.vue
+++ app.vue
@@ -1,19 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import Account from '@/pages/Account.vue'
-import { supabase } from './lib/supabaseClient.js'
 
-const session = ref()
-
-onMounted(() => {
-  supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
-
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
 </script>
 
 <template>
@@ -21,8 +7,7 @@ onMounted(() => {
     
     <nuxtLayout>
 
-        <Account v-if="session" :session="session" />
-        <nuxtPage v-else />
+        <nuxtPage />
 
       </nuxtLayout>
       
